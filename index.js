233502e86ef0dc951165ae188eef384537ec5363
HuntsberryS <huntsberry.shiloh@gmail.com>
2025-06-20 18:18:00 -0400

    form submit function + styling change
    added functionality so the form sends to my email. changed the present widths for a more responsive look. tweaked color palette. added skills and languages data

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
-// Clearing out the input fields on clicking the submit button
-document.querySelector(".submit-button").onclick = function() {submitForm()};
-
-const submitForm = () => {
-    document.getElementById("name").value = ""
-    document.getElementById("email").value = ""
-}
-
 
 const cursor = document.querySelector(".cursor");
 document.addEventListener("mousemove", (e) => {
-  cursor.style.left = `${e.pageX}px`;
-  cursor.style.top = `${e.pageY}px`;
+  cursor.style.left = `${e.clientX}px`;
+  cursor.style.top = `${e.clientY}px`;
 });
 
